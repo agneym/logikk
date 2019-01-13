da0ddf8422bc1e293f4e16af7e500c083a2d6d19
BoyWithSilverWings <agney@outlook.in>
2019-01-13 22:35:41 +0530

    rename to logikk

--- src/Evaluator.js
+++ src/Evaluator.js
@@ -44,6 +44,7 @@ class Evaluator extends React.Component {
         selectors
       });
     } catch (err) {
+      console.error(err);
       this.setState({
         error: err
       });
@@ -85,60 +86,82 @@ class Evaluator extends React.Component {
       }
     }
   }
+  renderError() {
+    return (
+      <div className="uk-section uk-dark uk-text-center">
+        <h5>Please enter a valid logical expression</h5>
+        <sub>
+          For eg: <pre className="uk-width-1-4 uk-margin-auto">a&&b</pre>
+        </sub>
+      </div>
+    );
+  }
   render() {
-    const { identifiers, matrix, selectors, representation } = this.state;
+    const {
+      identifiers,
+      matrix,
+      selectors,
+      representation,
+      error
+    } = this.state;
     return (
       <Fragment>
-        <div className="uk-flex uk-flex-right uk-margin-right uk-margin-bottom">
-          <select
-            className="uk-select uk-form-small uk-form-width-small"
-            value={representation}
-            onChange={this.changeRep}
-          >
-            <option value="bool">Boolean</option>
-            <option value="icon">Icons</option>
-          </select>
-        </div>
-        <div className="uk-overflow-auto result-table">
-          <table className="uk-table uk-table-hover uk-table-middle uk-text-center">
-            <thead>
-              <tr>
-                {identifiers.map((identifier, index) => (
-                  <th className="uk-text-center" key={index + identifier}>
-                    {identifier}
-                  </th>
-                ))}
-                <td className="uk-text-bold uk-text-primary uk-text-center">
-                  RESULT
-                </td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                {new Array(matrix[0] ? matrix[0].length : 0)
-                  .fill(1)
-                  .map((a, index) => {
-                    const id = `${identifiers[index] || "result"}-${index}`;
-                    return (
-                      <Selector
-                        key={index}
-                        name={id}
-                        value={selectors[id]}
-                        onChange={this.changeSelector}
-                      />
-                    );
-                  })}
-              </tr>
-              {matrix.map((row, index) => (
-                <tr key={index}>
-                  {row.map((value, index) => (
-                    <td key={index}>{this.renderRep(value)}</td>
+        {error ? (
+          this.renderError()
+        ) : (
+          <Fragment>
+            <div className="uk-flex uk-flex-right uk-margin-right uk-margin-bottom">
+              <select
+                className="uk-select uk-form-small uk-form-width-small"
+                value={representation}
+                onChange={this.changeRep}
+              >
+                <option value="bool">Boolean</option>
+                <option value="icon">Icons</option>
+              </select>
+            </div>
+            <div className="uk-overflow-auto result-table">
+              <table className="uk-table uk-table-hover uk-table-middle uk-text-center">
+                <thead>
+                  <tr>
+                    {identifiers.map((identifier, index) => (
+                      <th className="uk-text-center" key={index + identifier}>
+                        {identifier}
+                      </th>
+                    ))}
+                    <td className="uk-text-bold uk-text-primary uk-text-center">
+                      RESULT
+                    </td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    {new Array(matrix[0] ? matrix[0].length : 0)
+                      .fill(1)
+                      .map((a, index) => {
+                        const id = `${identifiers[index] || "result"}-${index}`;
+                        return (
+                          <Selector
+                            key={index}
+                            name={id}
+                            value={selectors[id]}
+                            onChange={this.changeSelector}
+                          />
+                        );
+                      })}
+                  </tr>
+                  {matrix.map((row, index) => (
+                    <tr key={index}>
+                      {row.map((value, index) => (
+                        <td key={index}>{this.renderRep(value)}</td>
+                      ))}
+                    </tr>
                   ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+                </tbody>
+              </table>
+            </div>
+          </Fragment>
+        )}
       </Fragment>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header className="uk-margin-medium-bottom uk-text-center">
-      <h1 className="uk-heading-line uk-margin-small">LOGIK</h1>
+      <h1 className="uk-heading-line uk-margin-small">LOGIKK</h1>
       <sub className="uk-text-meta">Logical Expression Visualiser</sub>
     </header>
   );
